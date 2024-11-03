47ebfb858af5f05a6c8e07e1f382ee304c82e27b
Teo <teo.bostiog@yahoo.com>
2024-11-03 17:15:09 +0200

    adjusted the square width

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -4,7 +4,7 @@ const Square = ({ value, onClick, disabled }) => (
   <button
     onClick={onClick}
     disabled={disabled}
-    className={`w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 text-3xl sm:text-4xl md:text-5xl font-bold flex items-center justify-center
+    className={`w-16 h-16 sm:w-24 sm:h-20 md:w-24 md:h-24 text-3xl sm:text-4xl md:text-5xl font-bold flex items-center justify-center
       ${disabled ? "opacity-50" : "hover:bg-white/5"}
       text-amber-500`}
   >
