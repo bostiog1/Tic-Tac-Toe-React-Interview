aaa741405c03a98a86e465f98ba5e616eaa2d5cc
Teo <teo.bostiog@yahoo.com>
2024-11-03 17:04:19 +0200

    added useEffect to update the localStorage when the game is finnished

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { useSelector } from "react-redux";
 import PlayerBoard from "./components/PlayerBoard";
 import ScoreBoard from "./components/ScoreBoard";
+import { useEffect } from "react";
 
 const App = () => {
+  const scores = useSelector((state) => state.game.scores);
+
+  // Effect to update localStorage whenever scores change
+  useEffect(() => {
+    localStorage.setItem("scores", JSON.stringify(scores));
+  }, [scores]);
+
   return (
     <div className="min-h-screen bg-black text-white">
       <ScoreBoard />
 
-      {/* Divider line between ScoreBoard and Player Boards */}
-      <div className="w-full border-t border-neutral-700 my-4"></div>
-
-      {/* Player boards */}
-      <div className="flex">
+      <div className="flex flex-col md:flex-row flex-grow">
         <PlayerBoard isPlayerX={true} />
         <PlayerBoard isPlayerX={false} />
       </div>
