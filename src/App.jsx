e81b3a00c8ea99d9fc59a5666fcbfc6b0ca20964
Teo <teo.bostiog@yahoo.com>
2024-11-04 13:52:29 +0200

    Deleted the useEffect that was setting the score inside localStorage from App.jsx, we have it in redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,16 @@
-import { useSelector } from "react-redux";
 import PlayerBoard from "./components/tic-tac-toe/PlayerBoard";
 import ScoreBoard from "./components/tic-tac-toe/ScoreBoard";
-import { useEffect } from "react";
-import ChatParent from "./components/chat-room/ChatParent";
+import Chat from "./components/chat-room/Chat";
 
 const App = () => {
-  const scores = useSelector((state) => state.game.scores);
-
-  // Effect to update localStorage whenever scores change
-  useEffect(() => {
-    localStorage.setItem("scores", JSON.stringify(scores));
-  }, [scores]);
-
   return (
     <div className="min-h-screen bg-black text-white">
       <ScoreBoard />
-
-      <div className="flex flex-col md:flex-row flex-grow">
+      <div className="flex flex-col md:flex-row justify-center items-center px-4 md:px-8">
         <PlayerBoard isPlayerX={true} />
         <PlayerBoard isPlayerX={false} />
       </div>
-      <ChatParent />
+      <Chat />
     </div>
   );
 };
